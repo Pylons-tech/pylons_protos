c322921faf3e93020f7c385d0d3bafbda55f3cb1
afti-githobo <31148582+afti-githobo@users.noreply.github.com>
2021-12-29 19:18:39 -0800

    Update internal_deploy.sh

--- internal_deploy.sh
+++ internal_deploy.sh
@@ -4,20 +4,20 @@ outdir=schema
 lang=jsonschema
 protopath=prototmp
 
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/tendermint/abci/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/tendermint/crypto/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/tendermint/types/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/tendermint/version/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/tendermint/abci/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/tendermint/crypto/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/tendermint/types/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/tendermint/version/*
 
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/auth/v1beta1/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/base/v1beta1/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/base/abci/v1beta1/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/base/query/v1beta1/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/crypto/multisig/v1beta1/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/crypto/ed25519/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/crypto/secp256k1/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/tx/signing/v1beta1/*
-protoc --proto_path=$protopath/ --${lang}_out=$outdir/ proto/cosmos/tx/v1beta1/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/auth/v1beta1/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/base/v1beta1/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/base/abci/v1beta1/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/base/query/v1beta1/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/crypto/multisig/v1beta1/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/crypto/ed25519/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/crypto/secp256k1/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/tx/signing/v1beta1/*
+protoc --proto_path=$protopath/ --${lang}_out=$outdir/ $protopath/cosmos/tx/v1beta1/*
 
 mv  -v ~$protopath/* ~proto/*
 
